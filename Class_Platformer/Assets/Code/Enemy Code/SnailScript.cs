0422510cd67603988c18ac5924f2fc7a710ce7ec
mginnetty <mginnetty@gmail.com>
2023-04-07 12:21:09 -0400

    Chapter 34

--- Class_Platformer/Assets/Code/Enemy Code/SnailScript.cs
+++ Class_Platformer/Assets/Code/Enemy Code/SnailScript.cs
@@ -10,37 +10,95 @@ public class SnailScript : MonoBehaviour
     private Rigidbody2D myBody;
     private Animator anim;
 
-    private bool moveLeft;
+    public LayerMask playerLayer;
 
-    public Transform down_Collision;
+    private bool moveLeft;
+    private bool canMove;
+    private bool stunned;
+    
+    public Transform left_Collision, right_Collision, top_Collision, down_Collision;
+    private Vector3 left_Collision_Position, right_Collision_Position;
 
 
     void Awake() {
         myBody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+
+        left_Collision_Position = left_Collision.position;
+        right_Collision_Position = right_Collision.position;
+        
     }
     
     
     void Start() {
         moveLeft = true;
+        canMove = true;
+        
     }
 
     
     void Update() {
-        if (moveLeft) {
-            myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
-        } else {
-            myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+
+        if (canMove) {
+            if (moveLeft) {
+                myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
+            } else {
+                myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+            } 
         }
+        
         
         CheckCollision();
         
     } // Update
 
     void CheckCollision() {
-        if (!Physics2D.Raycast(down_Collision.position, Vector2.down, 0.1f)) {
-            ChangeDirection();
+        RaycastHit2D leftHit = Physics2D.Raycast(left_Collision.position, Vector2.left, 0.1f, playerLayer);
+        RaycastHit2D rightHit = Physics2D.Raycast(right_Collision.position, Vector2.right, 0.1f, playerLayer);
+
+        Collider2D topHit = Physics2D.OverlapCircle(top_Collision.position, 0.2f, playerLayer);
+
+        if (topHit != null){
+            
+            if (topHit.gameObject.tag == MyTags.PLAYER_TAG){
+                 if (!stunned) {
+                      topHit.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(topHit.gameObject.GetComponent<Rigidbody2D>().velocity.x, 7f);
+                      canMove = false;
+                      myBody.velocity = new Vector2(0,0);
+                      anim.Play("Stunned");
+                      stunned = true;
+                
+                 } 
+            } 
         }
+        
+            if (leftHit){
+                if (leftHit.collider.gameObject.tag ==  MyTags.PLAYER_TAG){
+                    if (!stunned){
+                        // Apply Damage to Player
+                    } else {
+                       myBody.velocity = new Vector2(15f,myBody.velocity.y);
+                    }   
+                }
+            }
+            
+            if (rightHit) {
+                if (rightHit.collider.gameObject.tag == MyTags.PLAYER_TAG) {
+                    if (!stunned) {
+                        // Apply Player Damage
+                    } else {
+                        myBody.velocity = new Vector2(-15f,myBody.velocity.y);
+                    }   
+                }
+                
+            }    
+            
+            
+            if (!Physics2D.Raycast(down_Collision.position, Vector2.down, 0.1f)) {
+            ChangeDirection();
+            }
+            
+            
     }//CheckCollision
 
     void ChangeDirection() {
@@ -48,15 +106,17 @@ public class SnailScript : MonoBehaviour
         Vector3 tempscale = transform.localScale;
         if (moveLeft) {
             tempscale.x = Mathf.Abs(tempscale.x);
+            left_Collision.position = left_Collision_Position;
+            right_Collision.position = right_Collision_Position;
         } else {
             tempscale.x = -Math.Abs(tempscale.x);
+            left_Collision.position = right_Collision_Position;
+            right_Collision.position = left_Collision_Position;
         }
 
         transform.localScale = tempscale;
     }//ChangeDirection
 
-
-
- 
+    
 } //  SnailScript
 
